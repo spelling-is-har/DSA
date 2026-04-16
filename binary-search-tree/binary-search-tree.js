db22bdc914c94bf0bbe8b864c22f11d6d3655b43
Jonathan Winter <jonathan.winter@hotmail.com>
2026-04-16 14:33:27 +0100

    add method to check if a value is in the BST
    Changes to be committed:
    	modified:   binary-search-tree/binary-search-tree.js
    	modified:   binary-search-tree/binary-search-tree.test.js
    
    Changes not staged for commit:
    	deleted:    babel.config.js
    
    Untracked files:
    	.gitignore

--- binary-search-tree/binary-search-tree.js
+++ binary-search-tree/binary-search-tree.js
@@ -1,3 +1,5 @@
+import { arrayBuffer } from "node:stream/consumers"
+
 class Node {
     constructor() {
         this.value = null
@@ -8,10 +10,44 @@ class Node {
 
 export class Tree {
     constructor(arr) {
-        this.root = buildTree(arr)
+        if (!arr || arr.length === 0) throw new Error("Must initialise with a value")
+        this.root = this.#buildTree(arr)
     }
     print() {
         prettyPrint(this.root)
+    }
+    #buildTree(arr) {
+        return buildTree(arr)
+    }
+    contains(value) {
+        //if the BST is empty do not search is and return false
+        // if (this.root.left === null && this.root.right === null) return false
+        return containsValue(this.root, value)
+    }
+}
+
+function containsValue(root, value) {
+    //if the value is found return true
+    if (root.value === value) return true
+    //if the value is bigger than the value in the current node in the tree, then traverse
+    //right along the tree, if value is smaller than the current node then traverse left
+    if(value > root.value) {
+        //returns false if there is no node on the right
+        if (root.right === null) {
+            return false
+        } else {
+            return containsValue(root.right, value)
+        }
+    } else if (value < root.value) {
+        //returns false if there is no node on the left
+        if (root.left === null) {
+            return false
+        } else {
+            return containsValue(root.left, value)
+        }
+    //returns false if the value cannot be found
+    } else {
+        return false
     }
 }
 
@@ -27,17 +63,19 @@ function populateTree(arr, start, end) {
 
     const mid = start + Math.floor((end - start) / 2)
     const root = new Node(arr[mid])
+    //if there is no value, then set the value to null
     root.value = arr[mid]
 
     //make left and right branches
     root.left = populateTree(arr, start, mid - 1)
     root.right = populateTree(arr, mid + 1, end)
-
-    console.log(root)
+    
     return root
 }
 
 export function sanitiseArr(arr) {
+    //allows the user to initialise the BST with an array length of 0
+
     const noDuplicatesArr = [... new Set(arr)]
     return noDuplicatesArr.sort((a,b) => a - b)
 }
@@ -53,5 +91,6 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
 }
 
-const tree = new Tree([1,2,3,4,5,6,7])
-tree.print()
+// const tree = new Tree()
+// tree.print()
+// // console.log(tree.root)
--- binary-search-tree/binary-search-tree.test.js
+++ binary-search-tree/binary-search-tree.test.js
@@ -4,6 +4,7 @@ test("HashMap is defined", () => {
   expect(Tree).toBeDefined();
 });
 
+
 test("sanitiseArr() returns an array that does not need sanitation", () => {
   expect(sanitiseArr([0,1,2])).toStrictEqual([0,1,2]);
 });
@@ -19,3 +20,32 @@ test("sanitiseArr() removes duplicates", () => {
 test("sanitiseArr() removes duplicates and sorts", () => {
   expect(sanitiseArr([4,0,1,3,2,1])).toStrictEqual([0,1,2,3,4]);
 });
+
+
+test("Error is thrown if trying to initalise with an empty array", () => {
+    expect(() => {
+        const tree = new Tree([])
+    }).toThrow(Error)
+
+});
+
+test("contains() returns false if there is no matches in a tree with exactly balanced branches tree", () => {
+    const tree = new Tree([1,2,3])
+    expect(tree.contains(4)).toBe(false)
+})
+
+
+test("contains() returns false if there is no matches in an unbalanced tree", () => {
+    const tree = new Tree([1,2,3,4])
+    expect(tree.contains(0)).toBe(false)
+})
+
+test("contains() returns true if there is a match in a tree with exactly balanced branches tree", () => {
+    const tree = new Tree([1,2,3])
+    expect(tree.contains(2)).toBe(true)
+})
+
+test("contains() returns true if there is a match in a bigger tree with exactly balanced branches tree", () => {
+    const tree = new Tree([1,2,3,4,5])
+    expect(tree.contains(5)).toBe(true)
+})
